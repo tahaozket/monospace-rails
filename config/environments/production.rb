8faf4b1d38389d75bbcdf96db09b8612c2f97f84
Brian Collins <brian@stripe.com>
2013-02-11 20:11:57 +0000

    precompile some assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Monospace::Application.configure do
   # config.assets.manifest = YOUR_PATH
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( bootstrap.css monospace.css )
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
