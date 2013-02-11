4305881096dc730fcf36c5acd6452b58e2520949
Brian Collins <brian@stripe.com>
2013-02-11 19:42:45 +0000

    Handle card decline errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,11 @@ class UsersController < ApplicationController
     else
       render :action => :new
     end
+  rescue Stripe::CardError => e
+    @user.errors.add :base, e.message
+    @user.stripe_token = nil
+    render :action => :new
+
   rescue Stripe::StripeError => e
     logger.error e.message
     @user.errors.add :base, "There was a problem with your credit card"
